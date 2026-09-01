8718f78507610fd9b24328d383c91780f63adf42
h3rus4n15-maker <h3rus4n15@gmail.com>
2026-09-01 18:49:29 +0700

    pisahkan

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import LandingPage from './LandingPage.jsx'
 import './index.css'
 import './style.css'
 
-const pathname = window.location.pathname
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {pathname === '/landing' ? <LandingPage /> : <App />}
+    <App />
   </React.StrictMode>,
 )
